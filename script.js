aa5e069a9be339e646fd713c3c45e819c21f2680
ziadbd7000-a11y <ziadbd7000@gmail.com>
2025-11-20 16:56:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
-function generateCharacter() {
-    let skin = document.getElementById("skin").value;
-    let hair = document.getElementById("hair").value;
-    let eyes = document.getElementById("eyes").value;
-    
-    let resultText = `
-        الشخصية:
-        <br>لون البشرة: ${skin}
-        <br>لون الشعر: ${hair}
-        <br>لون العيون: ${eyes}
-    `;
+body {
+    background: #f4f4f4;
+    font-family: sans-serif;
+    text-align: center;
+}
 
-    document.getElementById("result").innerHTML = resultText;
+.title {
+    margin-top: 20px;
+    font-size: 26px;
+}
+
+#character-container {
+    margin-top: 30px;
+}
+
+#character svg {
+    width: 260px;
+}
+
+.controls {
+    margin-top: 25px;
+    background: white;
+    padding: 15px;
+    width: 280px;
+    margin: auto;
+    border-radius: 10px;
+    box-shadow: 0 0 8px #ccc;
+}
+
+.controls label {
+    display: block;
+    margin-top: 12px;
+    font-size: 15px;
 }
